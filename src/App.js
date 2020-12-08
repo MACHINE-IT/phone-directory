16c5d4e1bd8e936c56283473923604d803126470
Yogesh Kumar <kumaryogesh8490@gmail.com>
2020-12-08 20:39:21 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        Phonebook Directory
+        {/*<div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span>
+          <span>Phone Number</span>
+    </div>*/}
+    <label htmlFor="name">Name: </label>
+    <input id="name" type="text" placeholder="type here" defaultValue="Yogesh Kumar"/>    
       </div>
     );
   }
